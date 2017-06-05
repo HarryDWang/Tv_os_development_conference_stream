a3a7185ca2097b3f7a42ef113e314ae961a9b95f
harrydw <harrydw@umich.edu>
2017-06-05 10:19:06 -0400

    first commit

---
+++ TV_os_development_conference_stream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TV_os_development_conference_stream
+//
+//  Created by Harry Wang on 6/5/17.
+//  Copyright © 2017 Harry Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

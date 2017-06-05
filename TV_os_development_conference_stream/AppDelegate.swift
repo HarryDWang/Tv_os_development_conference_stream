da2a53aae6aaee3b7f8302b07380bd75798ee418
harrydw <harrydw@umich.edu>
2017-06-05 10:33:21 -0400

    added server communication to app delegate

--- TV_os_development_conference_stream/AppDelegate.swift
+++ TV_os_development_conference_stream/AppDelegate.swift
@@ -14,9 +14,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
 
     var window: UIWindow?
     
+    //handles communicating with server
     var appController: TVApplicationController?
     static let TVBaseURL = "http://localhost:9001/"
     static let TVBootURL = "\(AppDelegate.TVBaseURL)js/application.js"
+    
+    
+    //fills out the path to main javascript file and the root directory of the server
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        // 1
+        //initializes TVApplicationController; will fill this object with information
+        let appControllerContext = TVApplicationControllerContext()
+        
+        // 2
+        //fills context with the path to main Javascript file, and the root directory of server.
+        guard let javaScriptURL = URL(string: AppDelegate.TVBootURL) else { fatalError("unable to create NSURL") }
+        appControllerContext.javaScriptApplicationURL = javaScriptURL
+        appControllerContext.launchOptions["BASEURL"] = AppDelegate.TVBaseURL
+        
+        // 3
+        //will pull down root Javascript file and begin executing it.
+        appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
+        return true
+    }
+    
+    
 
 
 
